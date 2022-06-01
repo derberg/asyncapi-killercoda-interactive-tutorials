fbf05b812dd7a704acc407c55660ae8b44a9bb5a
derberg <lpgornicki@gmail.com>
2022-06-01 14:34:31 +0200

    fix: do foreground at the end

--- streetlight-tut/setup_node.sh
+++ streetlight-tut/setup_node.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "We need to install Nodejs and NPM first. Stay strong!"
 curl --silent --output /dev/null -sL https://deb.nodesource.com/setup_14.x | bash -
 apt-get install -y -q nodejs
-apt-get install -y -q npm
+apt install npm

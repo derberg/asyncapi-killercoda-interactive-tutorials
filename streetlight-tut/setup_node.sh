fa3a2e21e85b197556ac4b33ca6bf5dcd27016da
Lukasz Gornicki <lpgornicki@gmail.com>
2023-08-08 16:29:50 +0200

    fix: no need for explicit installation of npm

--- streetlight-tut/setup_node.sh
+++ streetlight-tut/setup_node.sh
@@ -5,7 +5,7 @@ echo "We need to install Nodejs and NPM first. Stay strong!"
 echo "#####################################################"
 curl --silent -sL https://deb.nodesource.com/setup_16.x -o /tmp/nodesource_setup.sh
 bash ../tmp/nodesource_setup.sh
-apt-get install -y -q nodejs npm
+apt-get install -y -q nodejs
 echo "#####################################################"
 echo "All set! Go ahead!"
 echo "#####################################################"

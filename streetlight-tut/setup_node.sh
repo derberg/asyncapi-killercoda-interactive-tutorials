c9edbb7c2085481d9a06c91c50af7cd844127c98
derberg <lpgornicki@gmail.com>
2023-10-23 14:16:20 +0200

    fix: rollback node

--- streetlight-tut/setup_node.sh
+++ streetlight-tut/setup_node.sh
@@ -3,7 +3,9 @@
 echo "#####################################################"
 echo "We need to install Nodejs and NPM first. Stay strong!"
 echo "#####################################################"
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash && source ~/.bashrc && nvm install 16
+curl --silent -sL https://deb.nodesource.com/setup_16.x -o /tmp/nodesource_setup.sh
+bash ../tmp/nodesource_setup.sh
+apt-get install -y -q nodejs
 echo "#####################################################"
 echo "All set! Go ahead!"
 echo "#####################################################"
